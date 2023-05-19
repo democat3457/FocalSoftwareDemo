c8e8b8758f8b3652380e5a0af5e22ccd5f48b69d
Colin Wong <cwongmath@gmail.com>
2023-05-19 03:15:00 -0500

    Initial Commit

---
+++ FocalSoftwareDemo/FocalSoftwareDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocalSoftwareDemoApp.swift
+//  FocalSoftwareDemo
+//
+//  Created by Colin Wong on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FocalSoftwareDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
